ab5f9e2de793552cf032c02174d75ce4654fa496
schutyb <schutyb@gmail.com>
2024-07-01 19:42:49 -0300

    add save option

--- janealia_samples.py
+++ janealia_samples.py
@@ -2,8 +2,13 @@ import tools
 import matplotlib.pyplot as plt
 from matplotlib import colors
 import numpy
+from PIL import Image 
 
-image = plt.imread("/Users/schutyb/Documents/Projects/rgb-phasors/data/fluoresence/janelias_samples/janelia_sample_60x_roi2.tif")
+image = plt.imread("/Users/schutyb/Documents/Projects/rgb-phasors/data/fluoresence/janelias_samples/janelia_sample_60x_roi2.tif")[600:1624, 350:1374]
+
+# im = Image.fromarray(image)
+# im.save("/Users/schutyb/Documents/Projects/rgb-phasors/data/fluoresence/janelias_samples/janelia_sample_60x_roi2_cut_roi.tif")
+
 im = tools.rgb2bgr(image)
 dc, g, s,  = tools.phasor(im)
 
@@ -21,7 +26,8 @@ g = tools.median_filter(g, 5)
 s = tools.median_filter(s, 5)
 
 centers = [[-0.2, 0.53], [-0.07, 0.22], [0.06, -0.02]]
-r = 0.15
+centers = [[0.2, -0.28], [-0.15, 0.43], [-0.003, 0.03]]
+r = 0.2
 
 g = numpy.where(mask, g, numpy.zeros(g.shape))
 s = numpy.where(mask, s, numpy.zeros(s.shape))
